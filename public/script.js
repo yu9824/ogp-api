b6026efeb62a896f3cb225fcd9268b5f7aa90a5b
yu9824 <58211916+yu9824@users.noreply.github.com>
2022-01-03 00:28:42 +0900

    Create sample form.

---
+++ public/script.js
@@ -0,0 +1,6 @@
+function get_ogp() {
+    const query = document.getElementById("ogp-api").value;
+    const url = "https://ogp-api-yu9824.vercel.app/api/ogp?url=" + query;
+    
+    window.open(url, "_blank");
+}
