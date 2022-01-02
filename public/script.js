8177b357dda169b53cb2117c72fc6300097ba087
yu9824 <58211916+yu9824@users.noreply.github.com>
2022-01-03 01:04:10 +0900

    Update repository name.

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 function get_ogp() {
-    const query = document.getElementById("ogp-api").value;
+    const query = document.getElementById("target-url").value;
     // const url = "https://ogp-api-yu9824.vercel.app/api/ogp?url=" + query;
     const url = "/api/ogp?url=" + query;
     
