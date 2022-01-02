fb4e288fb8485a33bd499887efa8d6997a724fdc
yu9824 <58211916+yu9824@users.noreply.github.com>
2022-01-03 00:54:51 +0900

    Update layout.

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 function get_ogp() {
     const query = document.getElementById("ogp-api").value;
-    const url = "https://ogp-api-yu9824.vercel.app/api/ogp?url=" + query;
+    // const url = "https://ogp-api-yu9824.vercel.app/api/ogp?url=" + query;
+    const url = "/api/ogp?url=" + query;
     
     window.open(url, "_blank");
 }
